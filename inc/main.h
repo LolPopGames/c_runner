88b8d7b480695e469586978d81ebf63e3692ad3d
LolPopGames <lpg13.artiom@gmail.com>
2024-09-14 22:21:57 +0300

    Added getch

--- inc/main.h
+++ inc/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 
 #include <stdio.h>
+#include <conio.h>
 #include "cmd.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -7,5 +7,12 @@ int main(int argc, char* argv[])
 
     make(file);
 
+    printf("y - run & delete\n");
+    printf("Y - run\n");
+    printf("n - delete\n");
+    printf("N - none\n");
+
+    char c = getch();
+
     return 0;
 }
